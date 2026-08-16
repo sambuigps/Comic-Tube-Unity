82db1189e941d5d3c4ffe418725105c854a3a618
Sukhman3125 <sukhman3125@gmail.com>
2026-08-17 03:56:57 +0530

    Feat: Add comic card

---
+++ Assets/Scripts/UI_Components/ComicCard.cs
@@ -0,0 +1,20 @@
+using TMPro;
+using UnityEngine;
+
+public class ComicCard : MonoBehaviour
+{
+    [SerializeField] WebImage coverImage;
+    [SerializeField] TMP_Text title;
+    [SerializeField] TMP_Text description;
+    [SerializeField] TMP_Text noOfStars;
+    [SerializeField] TMP_Text status;
+
+    public void Init(string coverUrl, string title, string description, int noOfStars, string status)
+    {
+        coverImage.Init(coverUrl);
+        this.title.SetText(title);
+        this.description.SetText(description);
+        this.noOfStars.SetText(noOfStars.ToString());
+        this.status.SetText(status);
+    }
+}
