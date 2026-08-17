e9362b46554105a4d6cd3affa604470759ab72bd
Sukhman3125 <sukhman3125@gmail.com>
2026-08-17 10:27:50 +0530

    remove(description)!: Remove description from Comic Card

--- Assets/Scripts/UI_Components/ComicCard.cs
+++ Assets/Scripts/UI_Components/ComicCard.cs
@@ -9,11 +9,10 @@ public class ComicCard : MonoBehaviour
     [SerializeField] TMP_Text noOfStars;
     [SerializeField] TMP_Text status;
 
-    public void Init(string coverUrl, string title, string description, int noOfStars, string status)
+    public void Init(string coverUrl, string title, int noOfStars, string status)
     {
         coverImage.Init(coverUrl);
         this.title.SetText(title);
-        this.description.SetText(description);
         this.noOfStars.SetText(noOfStars.ToString());
         this.status.SetText(status);
     }
